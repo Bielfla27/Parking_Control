18f8713a099d19c4f5c5432b17142a4f429c1f28
Bielfla27 <gabibielmft@outlook.com>
2023-01-16 20:53:24 -0300

    Configurando o security

---
+++ src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.parkingcontrol.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+		.httpBasic()
+		.and()
+		.authorizeHttpRequests()
+		.anyRequest().authenticated();
+	}
+	
+}
