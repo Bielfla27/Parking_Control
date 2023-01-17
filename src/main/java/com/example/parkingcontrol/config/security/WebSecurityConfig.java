ba576b0aa569085d80c12b0f3075fa3b096aaded
Bielfla27 <gabibielmft@outlook.com>
2023-01-17 19:58:43 -0300

    Passando uma senha padrão salva em memoria

--- src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.parkingcontrol.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -16,6 +20,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated()
 		.and()
 		.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+			.withUser("Gabriel")
+			.password(passwordEncoder().encode("fofao"))
+			.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
