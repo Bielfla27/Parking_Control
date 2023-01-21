a4b924e38ea770ecf1c6c0ce12dca364e18c748e
Bielfla27 <gabibielmft@outlook.com>
2023-01-21 15:45:57 -0300

    Criando altenticação passando diretamente no controller

--- src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfigv2.java
+++ src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfigv2.java
@@ -3,12 +3,14 @@ package com.example.parkingcontrol.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigv2 {
 
 	@Bean
--- src/main/java/com/example/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/example/parkingcontrol/controller/ParkingSpotController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,6 +38,7 @@ public class ParkingSpotController {
 		this.parkingSpotService = parkingSpotService;
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){ //salvaEstacionamento
 		if(parkingSpotService.existsBylicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
@@ -56,11 +58,14 @@ public class ParkingSpotController {
 		
 	}
 	
+
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN' , 'ROLE_USER')")
 	@GetMapping
 	public ResponseEntity<List<ParkingSpotModel>> getAllParkingSports(){
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findyAll());
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN' , 'ROLE_USER')")
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id){
 		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findyById(id);
@@ -70,6 +75,7 @@ public class ParkingSpotController {
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id){
 		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findyById(id);
@@ -80,6 +86,7 @@ public class ParkingSpotController {
 		return ResponseEntity.status(HttpStatus.OK).body("Vaga deletada com sucesso");
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id,
 													@RequestBody @Valid ParkingSpotDto parkingSpotDto){
